5a43103e3a4afab83ef7c9aba04ae81726bbe948
Dharmendhar <dharmendhar.p@gmail.com>
2014-07-05 09:12:11 -0700

    All the compulsory stories have been completed

--- app/build/generated/source/r/debug/com/codepath/apps/basictwitter/R.java
+++ app/build/generated/source/r/debug/com/codepath/apps/basictwitter/R.java
@@ -19,40 +19,52 @@ public final class R {
         public static final int ic_home=0x7f020001;
         public static final int ic_launcher=0x7f020002;
         public static final int ic_mention=0x7f020003;
-        public static final int ptr_pulltorefresh_arrow=0x7f020004;
+        public static final int ic_profile=0x7f020004;
+        public static final int ic_twitter=0x7f020005;
+        public static final int ptr_pulltorefresh_arrow=0x7f020006;
     }
     public static final class id {
-        public static final int action_compose=0x7f080011;
-        public static final int action_settings=0x7f080010;
+        public static final int action_compose=0x7f080018;
+        public static final int action_settings=0x7f080017;
         public static final int btnCompose=0x7f080005;
         public static final int btnConnect=0x7f080007;
-        public static final int flContainer=0x7f080009;
+        public static final int flContainer=0x7f080010;
+        public static final int followers=0x7f08000d;
+        public static final int following=0x7f08000e;
+        public static final int fragmentUserTimeline=0x7f080009;
         public static final int imgUserCur=0x7f080001;
-        public static final int ivProfileImage=0x7f08000c;
-        public static final int lot=0x7f08000b;
+        public static final int item1=0x7f080019;
+        public static final int ivMyImage=0x7f08000a;
+        public static final int ivProfileImage=0x7f080013;
+        public static final int lot=0x7f080012;
         public static final int lout=0x7f080000;
-        public static final int lvTweets=0x7f08000a;
-        public static final int rlLayout=0x7f080008;
-        public static final int timeStamp=0x7f08000f;
-        public static final int tvBody=0x7f08000e;
-        public static final int tvScreenName=0x7f08000d;
+        public static final int lvTweets=0x7f080011;
+        public static final int relativeLayout=0x7f080008;
+        public static final int rlLayout=0x7f08000f;
+        public static final int timeStamp=0x7f080016;
+        public static final int tvBody=0x7f080015;
+        public static final int tvName=0x7f08000b;
+        public static final int tvScreenName=0x7f080014;
         public static final int txtCompose=0x7f080004;
         public static final int txtCount=0x7f080006;
         public static final int txtScreenName=0x7f080003;
+        public static final int txtStatus=0x7f08000c;
         public static final int txtUserName=0x7f080002;
     }
     public static final class layout {
         public static final int activity_compose=0x7f030000;
         public static final int activity_login=0x7f030001;
-        public static final int activity_timeline=0x7f030002;
-        public static final int fragment_tweets_list=0x7f030003;
-        public static final int tweet_layout=0x7f030004;
+        public static final int activity_profile=0x7f030002;
+        public static final int activity_timeline=0x7f030003;
+        public static final int fragment_tweets_list=0x7f030004;
+        public static final int tweet_layout=0x7f030005;
     }
     public static final class menu {
         public static final int compose=0x7f070000;
         public static final int login=0x7f070001;
         public static final int photos=0x7f070002;
-        public static final int timeline=0x7f070003;
+        public static final int profile=0x7f070003;
+        public static final int timeline=0x7f070004;
     }
     public static final class string {
         public static final int action_compose=0x7f050000;
@@ -67,7 +79,8 @@ public final class R {
         public static final int ptr_release_to_refresh=0x7f050009;
         public static final int title_activity_compose=0x7f05000a;
         public static final int title_activity_photos=0x7f05000b;
-        public static final int title_activity_timeline=0x7f05000c;
+        public static final int title_activity_profile=0x7f05000c;
+        public static final int title_activity_timeline=0x7f05000d;
     }
     public static final class style {
         /** 
---
+++ app/src/main/java/com/codepath/apps/basictwitter/fragments/UserTimelineFragment.java
@@ -0,0 +1,44 @@
+package com.codepath.apps.basictwitter.fragments;
+
+import android.os.Bundle;
+import android.util.Log;
+
+import com.codepath.apps.basictwitter.TwitterClient;
+import com.codepath.apps.basictwitter.TwitterClientApp;
+import com.codepath.apps.basictwitter.models.Tweet;
+import com.loopj.android.http.JsonHttpResponseHandler;
+
+import org.json.JSONArray;
+
+/**
+ * Created by dharm on 7/4/14.
+ */
+public class UserTimelineFragment extends TweetsListFragment {
+    private TwitterClient client;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        client = TwitterClientApp.getRestClient();
+
+        populateTimeLine();
+    }
+
+    public void populateTimeLine(){
+
+        client.getMyTweets(new JsonHttpResponseHandler(){
+            @Override
+            public void onSuccess(JSONArray json) {
+                addAll(Tweet.fromJson(json));
+                Log.d("Debug", json.toString());
+
+            }
+
+            @Override
+            public void onFailure(Throwable e, String s){
+                Log.d("Debug",e.toString());
+
+            }
+        });
+    }
+}
